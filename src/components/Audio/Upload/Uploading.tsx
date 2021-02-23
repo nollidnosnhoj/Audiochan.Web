d0c8da607341b0046c7347c775b55897cdab48c2
Dillon Johnson <nollidnosnhoj@gmail.com>
2021-02-22 15:02:37 -1000

    Update Uploading.tsx
    extract certain code into its own inner functions.

--- src/components/Audio/Upload/Uploading.tsx
+++ src/components/Audio/Upload/Uploading.tsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { Box, Flex, Heading, Progress, Stack, Text } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Router from "next/router";
 import { addAudioPicture, useCreateAudio } from "~/lib/services/audio";
 import useUser from "~/lib/contexts/user_context";
 import { AudioRequest, CreateAudioRequest } from "~/lib/types/audio";
-import { objectToFormData } from "~/utils";
 import api from "~/utils/api";
 import { successfulToast } from "~/utils/toast";
+import { isAxiosError } from "~/utils/axios";
+import { ErrorResponse } from "~/lib/types";
 
 interface AudioUploadingProps {
   file: File;
@@ -31,101 +32,142 @@ export default function AudioUploading(props: AudioUploadingProps) {
   const { file, form, picture } = props;
   const { user } = useUser();
   const [stage, setStage] = useState(0);
-  const [progress, setProgress] = useState(0);
+  const [uploadAudioProgress, setUploadAudioProgress] = useState(0);
   const [audioId, setAudioId] = useState(0);
   const [completed, setCompleted] = useState(false);
   const [error, setError] = useState("");
   const { mutateAsync: createAudio } = useCreateAudio();
 
-  useEffect(() => {
-    if (file && user) {
-      // Get Pre-Signed Url from Server
+  const getUploadUrl = useCallback(() => {
+    return new Promise<S3PresignedUrl>((resolve, reject) => {
       api
         .post<S3PresignedUrl>("upload", { fileName: file.name })
         .then(({ data }) => {
-          setStage(1);
-          // Upload binary file using the provided upload link
-          const { url, uploadId } = data;
-          axios
-            .put(url, file, {
-              headers: {
-                "Content-Type": file.type,
-                "x-amz-meta-userId": `${user.id}`,
-              },
-              onUploadProgress: (evt) => {
-                const currentProgress = (evt.loaded / evt.total) * 100;
-                setProgress(currentProgress);
-              },
-            })
-            .then(() => {
-              setStage(2);
-              // Get additional audio information from audio element,
-              // and send a request to the server to create audio using
-              // the provided upload id
-              var audio = new Audio();
-              audio.src = window.URL.createObjectURL(file);
-              audio.onloadedmetadata = () => {
-                const body: CreateAudioRequest = {
-                  uploadId: uploadId,
-                  fileName: file.name,
-                  duration: Math.round(audio.duration),
-                  fileSize: file.size,
-                  ...(form ?? {
-                    title: "",
-                    description: "",
-                    tags: [],
-                    isPublic: true,
-                    isLoop: false,
-                  }),
-                };
-
-                createAudio(body)
-                  .then(({ id }) => {
-                    setAudioId(id);
-                    if (picture) {
-                      setStage(3);
-                    } else {
-                      setCompleted(true);
-                    }
-                  })
-                  .catch((err) => {
-                    // TODO: Add logging
-                    setError(
-                      "Unable to create audio. Please contact administrators."
-                    );
-                  });
-              };
-            })
-            .catch(() => {
-              // TODO: Add logging
-              setError(
-                "Unable to upload audio. Please contact administrators."
-              );
-            });
+          resolve(data);
         })
-        .catch(() => {
-          // TODO: Add logging
-          setError("Unable to upload audio. Please contact administrators.");
+        .catch((err) => {
+          const errorMessage = "Unable to upload audio.";
+          if (isAxiosError<ErrorResponse>(err)) {
+            reject(err.response?.data.message ?? errorMessage);
+          } else {
+            reject(errorMessage);
+          }
         });
-    }
+    });
   }, [file]);
 
-  // Once the audio is created (audioId is provided)
-  // Upload picture if provided.
+  const uploadAudioToS3 = useCallback(
+    (s3Url: string) => {
+      setStage(1);
+      return new Promise<void>((resolve, reject) => {
+        axios
+          .put(s3Url, file, {
+            headers: {
+              "Content-Type": file.type,
+              "x-amz-meta-userId": `${user!.id}`,
+              "x-amz-meta-originalFilename": `${file.name}`,
+            },
+            onUploadProgress: (evt) => {
+              const currentProgress = (evt.loaded / evt.total) * 100;
+              setUploadAudioProgress(currentProgress);
+            },
+          })
+          .then(() => {
+            resolve();
+          })
+          .catch(() => {
+            reject("Unable to upload audio.");
+          });
+      });
+    },
+    [file, user]
+  );
+
+  const requestCreateAudioFromServer = useCallback(
+    (uploadId: string) => {
+      setStage(2);
+      return new Promise<number>((resolve, reject) => {
+        const audio = new Audio();
+        audio.src = window.URL.createObjectURL(file);
+        audio.onloadedmetadata = () => {
+          const body: CreateAudioRequest = {
+            uploadId: uploadId,
+            fileName: file.name,
+            duration: Math.round(audio.duration),
+            fileSize: file.size,
+            ...(form ?? {
+              title: "",
+              description: "",
+              tags: [],
+              isPublic: true,
+              isLoop: false,
+            }),
+          };
+
+          createAudio(body)
+            .then(({ id }) => {
+              resolve(id);
+            })
+            .catch((err) => {
+              // TODO: Add logging
+              const errorMessage = "Unable to create audio";
+              if (isAxiosError<ErrorResponse>(err)) {
+                reject(err.response?.data.message ?? errorMessage);
+              } else {
+                reject(errorMessage);
+              }
+            });
+        };
+      });
+    },
+    [file]
+  );
+
+  const addPictureAfterAudioCreation = useCallback(() => {
+    return new Promise<string>((resolve) => {
+      if (audioId > 0 && picture) {
+        addAudioPicture(audioId, picture)
+          .then(({ data }) => {
+            resolve(data.image);
+          })
+          .catch((err) => {
+            // TODO: Add warning toast notifying that the image cannot be uploaded, but still resolve the promise.
+            resolve("");
+          });
+      } else {
+        resolve("");
+      }
+    });
+  }, [audioId, picture]);
+
+  // The process of creating the audio
+  useEffect(() => {
+    (async () => {
+      try {
+        if (file && user) {
+          const { url, uploadId } = await getUploadUrl();
+          await uploadAudioToS3(url);
+          const createdAudioId = await requestCreateAudioFromServer(uploadId);
+          setAudioId(createdAudioId);
+        }
+      } catch (err) {
+        setError(err);
+      }
+    })();
+  }, [file, user]);
+
+  // When the audio is created, upload the picture if applicable
   useEffect(() => {
     if (audioId > 0 && picture) {
-      addAudioPicture(audioId, picture)
-        .catch((err) => {
-          // TODO: Add Logging
-        })
-        .finally(() => {
-          setCompleted(true);
-        });
+      addPictureAfterAudioCreation().finally(() => {
+        setCompleted(true);
+      });
+    } else if (audioId > 0) {
+      setCompleted(true);
     }
   }, [audioId, picture]);
 
-  // Once the state of the uploading process is completed,
-  // Push the user into the audio detail page.
+  // Once completed, push the route to the newly created audio
   useEffect(() => {
     if (completed && audioId > 0) {
       Router.push(`audios/${audioId}`).then(() => {
@@ -157,7 +199,7 @@ export default function AudioUploading(props: AudioUploadingProps) {
           {!completed && (
             <Progress
               hasStripe
-              value={progress}
+              value={uploadAudioProgress}
               isAnimated
               isIndeterminate={stage !== 1}
             />
